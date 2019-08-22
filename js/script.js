db83b1b9bdca05120f5dd0882633089bb85e054f
aerounwired <>
2019-08-22 23:20:26 +0530

    host1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var owl = $('.owl-carousel');
+    owl.owlCarousel({
+      items: 4,
+      loop: true,
+      margin: 10,
+      responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    },
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true
+    });
+    $('.play').on('click', function () {
+      owl.trigger('play.owl.autoplay', [2000])
+    })
+    $('.stop').on('click', function () {
+      owl.trigger('stop.owl.autoplay')
+    })
+
+    
